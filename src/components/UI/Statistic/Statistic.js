363c316fa634293cd356d69d218c351e64d85534
MaDud <martaduda@interia.eu>
2020-02-06 14:01:58 +0100

    Adding statistic component

---
+++ src/components/UI/Statistic/Statistic.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import classes from './statistic.module.css';
+
+const statistic = props => {
+
+    let statisticMatric = props.matric.map( (el, index) => {
+        return <h1 key={index}>{el.value} <span>{el.description}</span></h1>
+    })
+
+    return (
+        <div className={classes.StatisticBox}>
+            {statisticMatric}
+        </div>
+    )
+};
+
+export default statistic;
--- src/containers/UserPanel/UserPanel.js
+++ src/containers/UserPanel/UserPanel.js
@@ -3,9 +3,15 @@ import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 
 import Table from "../../components/UI/Table/Table";
+import Statistic from '../../components/UI/Statistic/Statistic';
 
 const heads = ["Numer", "Nazwa stanowiska", "Właściciel", "Opcje"];
 const values = [[1,2,3,4,], [2,3,4,5]];
+
+const matric = [{value:30, description: "Aktywnych ocen ryzyka zawodowego"},
+                {value:5, description: "Wymagających przeglądu ocen ryzyka zawodowego"},
+                {value:0, description: "Przeterminowanych ocen ryzyka zawodowego"},
+                ];
 
 class UserPanel extends React.Component {
 
@@ -15,6 +21,7 @@ class UserPanel extends React.Component {
     render () {
         return (
             <Auxiliary>
+                <Statistic matric={matric}/>
                 <Table head={heads}
                         list={values}/>
             </Auxiliary>
