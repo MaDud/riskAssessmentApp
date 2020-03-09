8ca2c54695631ec51051c9f9f0ed034f58b87d55
MaDud <martaduda@interia.eu>
2020-03-09 12:25:18 +0100

    Adding tooltip to elementNavbar (UserPanel)

--- src/components/UserNav/ElementNavbar/ElementNavbar.js
+++ src/components/UserNav/ElementNavbar/ElementNavbar.js
@@ -8,16 +8,22 @@ const elementNavbar = props => {
     return(
         <div className={classes.ElementNavbar}>
             <Button btnType="Icon" 
-                    clicked={props.open}>
+                    clicked={props.open}
+                    className={classes.tooltip}>
                     <i className="fas fa-external-link-alt"></i>
+                    <span className={classes.tooltiptext}>Otwórz</span>
             </Button>
             <Button btnType="Icon" 
-                    clicked={props.edit}>
+                    clicked={props.edit}
+                    className={classes.tooltip}>
                     <i className="fas fa-edit"></i>
+                    <span className={classes.tooltiptext}>Edytuj</span>
             </Button>
             <Button btnType="Icon" 
-                    clicked={props.archive}>
+                    clicked={props.archive}
+                    className={classes.tooltip}>
                     <i className="fas fa-archive"></i>
+                    <span className={classes.tooltiptext}>Archiwizuj</span>
             </Button>
         </div>
     )
