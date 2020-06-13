14a1dc1d9c37b2044aa6b3ab51bcf6de569d1efa
MaDud <martaduda@interia.eu>
2020-06-13 15:30:44 +0200

    Changing table module to implement it in RAOutput

--- src/components/UI/Table/Table.js
+++ src/components/UI/Table/Table.js
@@ -10,13 +10,15 @@ const table = props => {
      const tableHead = props.columns.map( (head) => {
 
         let sortIcon = null;
-        if (head.id === props.sortOption.id) {
-            if (props.sortOption.sortType === 'asc') {
-                sortIcon = <FontAwesomeIcon icon='sort-up' className={classes.Sorted}/>;
+        if (props.sortable) {
+            if (head.id === props.sortOption.id) {
+                if (props.sortOption.sortType === 'asc') {
+                    sortIcon = <FontAwesomeIcon icon='sort-up' className={classes.Sorted}/>;
+                } else {
+                    sortIcon = <FontAwesomeIcon icon='sort-down' className={classes.Sorted}/>}
             } else {
-                sortIcon = <FontAwesomeIcon icon='sort-down' className={classes.Sorted}/>}
-        } else {
-            sortIcon = sortIcon = <FontAwesomeIcon icon='sort'/>
+                sortIcon = sortIcon = <FontAwesomeIcon icon='sort'/>
+            }
         }
 
         return <th key={head.id} id={head.id} onClick={props.sortable ? props.sort : null}>
@@ -28,39 +30,50 @@ const table = props => {
     );
 
     const data = props.rows;
-    let dataToSort = Object.values(data);
+    let tableList = Object.keys(data)
+            .map( row => {
+                return  <tr key={row} id={row} onClick={props.view}>
+                            {Object.keys(props.rows[row]).map( (cell, index) => {
+                            return <td key={index} headers={cell}>{props.rows[row][cell]}</td>})}
+                        </tr>
+        }); 
 
-    let sortedData = dataToSort.sort((a,b) => {
-        if ( typeof(a[props.sortOption.id]) === 'number') {
-            return a[props.sortOption.id] - b[props.sortOption.id]
+    if (props.sortable) {
+        let dataToSort = Object.values(data);
+
+        let sortedData = dataToSort.sort((a,b) => {
+            if ( typeof(a[props.sortOption.id]) === 'number') {
+                return a[props.sortOption.id] - b[props.sortOption.id]
+            }
+            if (a[props.sortOption.id] === undefined || b[props.sortOption.id] === undefined) {
+                return 1
+            } else {
+            return a[props.sortOption.id].toLowerCase().localeCompare(b[props.sortOption.id].toLowerCase())
+        }});
+
+        if (props.sortOption.sortType !== 'asc') {
+            sortedData.reverse()
         }
-        if (a[props.sortOption.id] === undefined || b[props.sortOption.id] === undefined) {
-            return 1
-        } else {
-        return a[props.sortOption.id].toLowerCase().localeCompare(b[props.sortOption.id].toLowerCase())
-    }});
 
-    if (props.sortOption.sortType !== 'asc') {
-        sortedData.reverse()
+        let dataOutput={};  
+        for (let el in sortedData) {
+            for (let sth in data) {
+                if (sortedData[el][props.sortOption.id] === data[sth][props.sortOption.id])
+                dataOutput[sth] = data[sth]
+            }
+        }
+
+        tableList = Object.keys(dataOutput)
+            .filter( (row,index) => {
+                return index >= (props.page-1)*props.range && index <= props.page*props.range-1})
+            .map( row => {
+                return  <tr key={row} id={row} onClick={props.view}>
+                            {Object.keys(props.rows[row]).map( (cell, index) => {
+                            return <td key={index} headers={cell}>{props.rows[row][cell]}</td>})}
+                        </tr>
+        });    
     }
 
-    let dataOutput={};  
-    for (let el in sortedData) {
-        for (let sth in data) {
-            if (sortedData[el][props.sortOption.id] === data[sth][props.sortOption.id])
-            dataOutput[sth] = data[sth]
-        }
-    }
-
-    const tableList = Object.keys(dataOutput)
-        .filter( (row,index) => {
-            return index >= (props.page-1)*props.range && index <= props.page*props.range-1})
-        .map( row => {
-            return  <tr key={row} id={row} onClick={props.view}>
-                        {Object.keys(props.rows[row]).map( (cell, index) => {
-                        return <td key={index} headers={cell}>{props.rows[row][cell]}</td>})}
-                    </tr>
-    });    
 
     return (
         <Auxiliary>
@@ -74,10 +87,11 @@ const table = props => {
                     {tableList}
                 </tbody>
             </table>
-            <Pagination data={props.rows}
+            {props.pagination ? <Pagination data={props.rows}
                         changePage={props.changePage}
                         page={props.page}
                         range={props.range}/>
+                        :null}
         </Auxiliary>
     )
 };
