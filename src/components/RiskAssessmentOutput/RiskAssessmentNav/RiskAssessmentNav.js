97f8821f6e3e1b6fff86f0eb006f5d9dc926574e
MaDud <martaduda@interia.eu>
2020-06-17 14:26:39 +0200

    Changing name of css module

--- src/components/RiskAssessmentOutput/RiskAssessmentNav/RiskAssessmentNav.js
+++ src/components/RiskAssessmentOutput/RiskAssessmentNav/RiskAssessmentNav.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import classes from './RiskAssessmentNav.module.css';
+import classes from './riskAssessmentNav.module.css';
 import Auxiliary from '../../../hoc/Auxiliary';
 
 const riskAssessmentNav = props => {
