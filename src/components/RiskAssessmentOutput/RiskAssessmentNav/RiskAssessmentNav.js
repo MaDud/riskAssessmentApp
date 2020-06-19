5431ecd08b00a9f0f21f885285a1960e024d6476
MaDud <martaduda@interia.eu>
2020-06-19 22:10:00 +0200

    Styling nav for RAOutput

--- src/components/RiskAssessmentOutput/RiskAssessmentNav/RiskAssessmentNav.js
+++ src/components/RiskAssessmentOutput/RiskAssessmentNav/RiskAssessmentNav.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import classes from './riskAssessmentNav.module.css';
 import Auxiliary from '../../../hoc/Auxiliary';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const riskAssessmentNav = props => {
     return (
         <Auxiliary>
             <ul className={classes.RANav}>
-                <li onClick={props.history}>Historia zmian</li>
-                <li onClick={props.edit}>Edytuj</li>
-                <li onClick={props.archive}>Archiwizuj</li>
-                <li onClick={props.close}>Zamknij</li>
+                <li onClick={props.history}><FontAwesomeIcon icon='folder-open' /><span>Historia zmian</span></li>
+                <li onClick={props.edit}><FontAwesomeIcon icon='edit' /><span>Edytuj</span></li>
+                <li onClick={props.archive}><FontAwesomeIcon icon='trash-alt' /><span>Archiwizuj</span></li>
             </ul>
         </Auxiliary>
     )
